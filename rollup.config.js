dfc6cb9585a43d221740d80272395dadc4bea7aa
gwansikk <seorit98@gmail.com>
2024-01-10 02:41:27 +0900

    chore: setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,31 @@
-import typescript from "@rollup/plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import dts from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
 
-const packageJson = require("./package.json");
+const pkg = require("./package.json");
 
 export default [
   {
     input: "src/index.ts",
     output: [
       {
-        file: packageJson.main,
+        file: pkg.main,
         format: "cjs",
         sourcemap: true,
       },
       {
-        file: packageJson.module,
-        format: "esm",
+        file: pkg.module,
+        format: "es",
         sourcemap: true,
       },
     ],
-    plugins: [resolve(), commonjs(), typescript()],
+    plugins: [resolve(), commonjs(), typescript(), terser()],
   },
   {
-    input: "dist/esm/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    input: "src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
     plugins: [dts.default()],
   },
 ];
