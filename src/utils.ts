1b35769f1dbc85b3068af8bd07d79e2b6c2f9ae6
gwansikk <seorit98@gmail.com>
2024-01-11 15:34:43 +0900

    feat: add endpoint utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,21 @@
-import { Interceptor } from "./types";
+export const log = (title: string, massage: string): void => {
+  console.warn('[chain]', `${title}: `, massage);
+};
 
-export const logging = (interceptor: Promise<any>): Promise<any> => {
-  return (
-    interceptor ||
-    console.warn("[chain]", "interceptor is not returning a response")
-  );
+export const createBaseURL = (url: string): string => {
+  const match = url.match(/^(https?:\/\/)?(.*)$/);
+  const protocol = match ? match[1] || 'https://' : 'https://';
+  let restOfUrl = match ? match[2] : url;
+
+  restOfUrl = restOfUrl.replace(/\/\/+/g, '/');
+
+  if (restOfUrl.endsWith('/')) {
+    restOfUrl = restOfUrl.substring(0, restOfUrl.length - 1);
+  }
+
+  return protocol + restOfUrl;
+};
+
+export const formatPath = (path: string): string => {
+  return path.replace(/^\//, '').replace(/\/+/g, '/');
 };
