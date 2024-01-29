e1460cc2b5aca0b988ca94aab1512c3ed3d3ea60
gwansikk <seorit98@gmail.com>
2024-01-30 02:28:47 +0900

    chore: build path configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,31 @@
-import typescript from "@rollup/plugin-typescript";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import dts from "rollup-plugin-dts";
-import terser from "@rollup/plugin-terser";
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import dts from 'rollup-plugin-dts';
+import terser from '@rollup/plugin-terser';
 
-const pkg = require("./package.json");
+import packageJson from './package.json';
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
-        file: pkg.main,
-        format: "cjs",
+        file: packageJson.main,
+        format: 'cjs',
         sourcemap: true,
       },
       {
-        file: pkg.module,
-        format: "es",
+        file: packageJson.module,
+        format: 'es',
         sourcemap: true,
       },
     ],
-    plugins: [resolve(), commonjs(), typescript(), terser()],
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), terser()],
   },
   {
-    input: "src/index.ts",
-    output: [{ file: "dist/index.d.ts", format: "es" }],
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
     plugins: [dts.default()],
   },
 ];
