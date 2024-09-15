f3cb8fbd447295c580dc9ecd18ba8b51840862fe
gwansikk <gwansik.kim@gwansik.dev>
2024-09-15 22:16:44 +0900

    test(react-query): add test case for queryFetchKey

--- packages/react-query/src/queryFetchKey.spec.ts
+++ packages/react-query/src/queryFetchKey.spec.ts
@@ -1,15 +1,9 @@
 import { fetchOptions } from '@query-fetch/core';
 import { queryFetchKey } from './queryFetchKey';
-
-type TResponseData = {
-  id: number;
-  title: string;
-  body: string;
-  userId: number;
-};
+import { queryOptions } from '@tanstack/react-query';
 
 function postsFetchOptions(id: number, filter?: 'latest' | 'popular') {
-  return fetchOptions<TResponseData>({
+  return fetchOptions({
     endpoint: ['posts', id],
     queryParameter: { filter },
   });
@@ -17,20 +11,29 @@ function postsFetchOptions(id: number, filter?: 'latest' | 'popular') {
 
 describe('queryFetchKey', () => {
   it('should generate the correct key', async () => {
-    const key = queryFetchKey(postsFetchOptions(1));
+    const key = queryFetchKey({ ...postsFetchOptions(1) });
+    const queryKey = queryOptions({ queryKey: key }).queryKey;
 
     expect(key).toEqual(['posts', 1]);
+    expect(queryKey).toEqual(['posts', 1]);
   });
 
   it('should generate the correct key with queryParameter', async () => {
     const key = queryFetchKey(postsFetchOptions(1, 'latest'));
+    const queryKey = queryOptions({ queryKey: key }).queryKey;
 
     expect(key).toEqual(['posts', 1, { filter: 'latest' }]);
+    expect(queryKey).toEqual(['posts', 1, { filter: 'latest' }]);
   });
 
   it('should generate the correct key with additionalKey', async () => {
-    const key = queryFetchKey(postsFetchOptions(1), ['additionalKey']);
+    const key = queryFetchKey({
+      ...postsFetchOptions(1),
+      queryKey: ['additionalKey', { id: 1 }],
+    });
+    const queryKey = queryOptions({ queryKey: key }).queryKey;
 
-    expect(key).toEqual(['posts', 1, 'additionalKey']);
+    expect(key).toEqual(['posts', 1, 'additionalKey', { id: 1 }]);
+    expect(queryKey).toEqual(['posts', 1, 'additionalKey', { id: 1 }]);
   });
 });
