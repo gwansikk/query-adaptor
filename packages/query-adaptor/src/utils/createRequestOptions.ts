5c5298cac8234516fec3a64c5ac4b3df2d3efd7d
gwansikk <gwansik.kim@gwansik.dev>
2024-10-23 07:11:05 +0900

    refactor(query-adaptor): simplify createRequestOptions function

--- packages/query-adaptor/src/utils/createRequestOptions.ts
+++ packages/query-adaptor/src/utils/createRequestOptions.ts
@@ -1,27 +1,20 @@
 import { FetchOptionsWithMethod } from '../fetchOptions';
-import type { TRequestOptions } from '../types';
+
 import { isContentTypeJson } from './isContentTypeJson';
 
-/**
- * Creates request options for a fetch call.
- *
- * @param {FetchOptionsWithMethod<TData, TBody>} options - The options for the fetch request, including method, headers, and body.
- * @returns {TRequestOptions} The formatted request options, including method, headers, and body.
- */
-export function createRequestOptions<TData, TBody>(
-  options: FetchOptionsWithMethod<TData, TBody>
-): TRequestOptions {
+export function createRequestOptions<TBody>(options: FetchOptionsWithMethod<TBody>): RequestInit {
   const isJson = isContentTypeJson(options.body);
+  const headers = new Headers();
 
-  const requestOptions: TRequestOptions = {
-    ...options,
+  if (isContentTypeJson(options.body)) {
+    headers.set('Content-Type', 'application/json');
+  }
+
+  const requestOptions: RequestInit = Object.assign(options, {
     method: options.method,
-    headers: {
-      'Content-Type': isJson ? 'application/json' : '',
-      ...options.options?.headers,
-    },
+    headers: headers,
     body: isJson ? JSON.stringify(options.body) : (options.body as BodyInit),
-  };
+  });
 
   return requestOptions;
 }
